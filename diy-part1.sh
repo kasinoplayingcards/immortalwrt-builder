bd607210db0686fb25bde3eb0a2fd64e64095090
kasinoplayingcards <126845560+kasinoplayingcards@users.noreply.github.com>
2026-05-22 09:59:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,8 +6,10 @@ rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/v2ray-core
 rm -rf feeds/packages/net/sing-box
 
-# 强制关闭 IPv6
+# 强制关闭 IPv6（编译期）
 sed -i 's/CONFIG_IPV6=y/CONFIG_IPV6=n/g' .config || true
 
-# 改默认 IP
+# 默认 IP 改为 192.168.11.1
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+
+echo "diy-part1 done"
