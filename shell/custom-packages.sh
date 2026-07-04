a6beae7bda066983cda16f164493f713e06cf3a0
immortalwrt-builder <126845560+kasinoplayingcards@users.noreply.github.com>
2026-07-04 11:39:51 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -1,1 +1,32 @@
+CUSTOM_PACKAGES="
 
+luci
+luci-base
+luci-compat
+rpcd-mod-luci
+
+luci-mod-admin-full
+luci-mod-network
+luci-mod-status
+luci-mod-system
+
+luci-app-firewall
+luci-app-opkg
+luci-app-ttyd
+
+# =====================
+# Network features
+# =====================
+luci-app-upnp
+p910nd
+
+# =====================
+# Proxy layer
+# =====================
+daed
+luci-app-daed
+luci-i18n-daed-zh-cn
+
+luci-app-openclash
+luci-i18n-openclash-zh-cn
+"
